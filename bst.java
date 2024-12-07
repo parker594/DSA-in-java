e6c02bb602041f8404950279fb98dda4d689a3a8
Debajyoti upadhayaya <debajyotiupadhyaya@gmail.com>
2024-12-07 18:27:30 +0530

    hlo

---
+++ bst.java
@@ -0,0 +1,43 @@
+
+public class bst{
+   static class Node{
+int data;
+Node left;
+Node right;
+Node(int data){
+    this.data=data; 
+}
+   }
+public static Node insert(Node root,int val){
+    if (root==null){
+        root = new Node(val);
+        return root;
+    }
+    if(root.data> val){git
+        root.left=insert(root.left,val);
+
+    }
+    else{
+        root.right=insert(root.right,val);
+    }
+    return root;
+}
+   
+   public static void inorder(Node root){
+    if (root==null){
+        return ;
+    }
+    inorder(root.left);
+    System.out.print(root.data+" ");
+    inorder(root.right);
+   }
+    public static void main(String args []){
+       int values []={2,3,5,7,9,4,6};
+       Node root = null;
+       for (int i=0;i<values.length ;i++){
+       root = bst.insert(root,values[i]); 
+       }
+bst.inorder(root);
+System.out.println();
+}
+}
